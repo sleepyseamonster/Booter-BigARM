6ffd502521bc3fa1d3a99f2c9d53b06fff296cc3
Kirk Artman <176690449+sleepyseamonster@users.noreply.github.com>
2026-08-24 17:21:23 -0700

    test: align startup staging with async terrain

--- Assets/_Project/Tests/Editor/TopDown3DPerformanceTests.cs
+++ Assets/_Project/Tests/Editor/TopDown3DPerformanceTests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Reflection;
 using BooterBigArm.TopDown3D;
 using NUnit.Framework;
@@ -16,7 +17,7 @@ namespace BooterBigArm.Tests
             "Assets/_Project/Materials/TopDown3D/Greybox_Rock.mat";
 
         [Test]
-        public void StartupBuildsImmediateTerrainButDefersNonCenterDecoration()
+        public void StartupQueuesImmediateTerrainWithoutSynchronousMeshConstruction()
         {
             var settings = AssetDatabase.LoadAssetAtPath<TopDown3DWorldSettings>(WorldSettingsPath);
             var terrainMaterial = AssetDatabase.LoadAssetAtPath<Material>(TerrainMaterialPath);
@@ -30,6 +31,8 @@ namespace BooterBigArm.Tests
             try
             {
                 targetObject.AddComponent<CapsuleCollider>();
+                var targetBody = targetObject.AddComponent<Rigidbody>();
+                targetBody.useGravity = false;
                 var world = worldObject.AddComponent<TopDown3DProceduralWorld>();
                 world.Configure(settings, targetObject.transform, terrainMaterial, rockMaterial);
 
@@ -39,32 +42,32 @@ namespace BooterBigArm.Tests
                 var immediateChunkCount = immediateDiameter * immediateDiameter;
                 var streamingDiameter = settings.StreamingRadius * 2 + 1;
                 var streamingChunkCount = streamingDiameter * streamingDiameter;
-                Assert.That(world.LoadedChunkCount, Is.EqualTo(immediateChunkCount));
-                Assert.That(world.DecoratedChunkCount, Is.EqualTo(1));
-                Assert.That(world.PendingDecorationCount, Is.EqualTo(immediateChunkCount - 1));
+                Assert.That(world.LoadedChunkCount, Is.Zero);
+                Assert.That(world.DecoratedChunkCount, Is.Zero);
+                Assert.That(world.PendingDecorationCount, Is.Zero);
+                Assert.That(world.PendingTerrainChunkCount, Is.EqualTo(streamingChunkCount));
+                Assert.That(GetPrivateCollectionCount(world, "terrainRequests"), Is.EqualTo(immediateChunkCount));
                 Assert.That(
-                    world.PendingTerrainChunkCount,
+                    GetPrivateCollectionCount(world, "pendingChunks"),
                     Is.EqualTo(streamingChunkCount - immediateChunkCount));
-
-                InvokePrivate(world, "ProcessPendingChunks", 1);
-
-                Assert.That(world.LoadedChunkCount, Is.EqualTo(immediateChunkCount));
-                Assert.That(world.DecoratedChunkCount, Is.EqualTo(2));
-                Assert.That(world.PendingDecorationCount, Is.EqualTo(immediateChunkCount - 2));
-
-                targetObject.transform.position += Vector3.right * settings.ChunkSize;
-                InvokePrivate(world, "RefreshChunks", false);
-                var pendingAfterCenterChange = world.PendingDecorationCount;
-                InvokePrivate(world, "RefreshChunks", false);
-
-                Assert.That(world.PendingDecorationCount, Is.EqualTo(pendingAfterCenterChange));
-                Assert.That(pendingAfterCenterChange, Is.EqualTo(immediateChunkCount - 2));
+                Assert.That(targetBody.isKinematic, Is.True);
             }
             finally
             {
                 Object.DestroyImmediate(worldObject);
                 Object.DestroyImmediate(targetObject);
             }
+        }
+
+        private static int GetPrivateCollectionCount(TopDown3DProceduralWorld world, string fieldName)
+        {
+            var field = typeof(TopDown3DProceduralWorld).GetField(
+                fieldName,
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.That(field, Is.Not.Null, $"Missing staged-work collection {fieldName}.");
+            var collection = field.GetValue(world) as ICollection;
+            Assert.That(collection, Is.Not.Null, $"Staged-work field {fieldName} is not a collection.");
+            return collection.Count;
         }
 
         private static void InvokePrivate(
